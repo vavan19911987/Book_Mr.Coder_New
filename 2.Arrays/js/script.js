85aff1698d8298a7755b42ca8e95ed19c2272bff
Ms.L_Ubuntu <v.pulin@yahoo.com>
2024-06-21 18:14:09 +0500

    Введение по массивом оконченно

--- 2.Arrays/js/script.js
+++ 2.Arrays/js/script.js
@@ -1,8 +1,28 @@
-'use strict'
+'use strict';
 
 
+// ? Создайте массив с числами. Прибавьте к каждому элементу массива число 3.
+// ? Выведите на экран измененный массив.
+
+//  let arr = [1,2,3,4,5,6,7,8,9,10];
+//  let modifiedArr = [];
+//  for (const number of arr) {
+//      modifiedArr.push(number + 3);
+//  }
+//  console.log(modifiedArr);
 
 
+// ? Код должен вывести последний элемент массива:
 
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length -1]);
 
+// ? Код должен найти сумму элементов массива:
 
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// ? Код должен вывести длину массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
