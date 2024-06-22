3f0d09f2f634ef509b71827386b30cfc5caccdbb
Ms.L_Ubuntu <v.pulin@yahoo.com>
2024-06-22 23:40:35 +0500

    Продолжение изучение обьектов

--- 2.Arrays/js/script.js
+++ 2.Arrays/js/script.js
@@ -26,3 +26,4 @@
 
 // let arr = [1, 2, 3, 4, 5];
 // console.log(arr.length);
+
--- 3.Objects/js/script.js
+++ 3.Objects/js/script.js
@@ -1,4 +1,59 @@
-'use strict'
+'use strict';
+
+// let obj = {1: 'a', 2: 'b', 3: 'c'};
+// console.log(obj);
+//
+// let a = 2
+// let b = 2
+// console.log(a + b);
+
+// let obj = {
+//     name: 'John',
+//     surname: 'Leo',
+//     patronymic: '11'
+// }
+//
+// console.log(obj['name']);
+// console.log(obj['surname']);
+// console.log(obj['patronymic']);
+// console.log(obj.name);
+// console.log(obj.surname);
+// console.log(obj.patronymic);
+
+//
+// let obj = {
+//     '1a': 1,
+//     'b2': 2,
+//     'c-c': 3,
+//     'd 4': 4,
+//     'e5': 5
+// };
+//
+// console.log(obj['1a']);
+// console.log(obj.b2);
+// console.log(obj['c-c']);
+// console.log(obj['d 4']);
+// console.log(obj.e5);
+
+
+let obj = {
+    x: 1,
+    y: 2,
+    z: 3
+};
+
+obj.x *= 2;
+obj.y *= 2;
+obj.z *= 2;
+console.log(obj);
+
+
+
+
+
+
+
+
 
 
 
