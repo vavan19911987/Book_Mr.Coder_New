1aaeff8a3ce199841bfe8fb08b4593bc0fd09398
Ms.L_Ubuntu <v.pulin@yahoo.com>
2024-07-01 19:39:13 +0500

    Половина циклов пройдена

--- 5.Cycles/js/script.js
+++ 5.Cycles/js/script.js
@@ -14,7 +14,67 @@
 // console.log(num);
 // console.log(i);
 
+// ? Дан следующий массив:
+// ? Выведите в консоль те элементы массива, которые больше 3-х, но меньше 10.
 
-for (let i = 100; i >= 0; i--) {
-    console.log(i)
+// let arr = [2, 5, 9, 15, 1, 4];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= 3 && arr[i] <= 10) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// ? Дан следующий объект:
+// ? Выведите в консоль те элементы объекта, значения которых - нечетные числа.
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5
+};
+// let keys = Object.values(obj);
+// for (let i = 0; i < keys.length; i++) {
+//     if (keys[i] % 2 === 1) {
+//         console.log(keys[i]);
+//     }
+// }
+
+// let keys = Object.keys(obj); // Получаем массив ключей объекта
+//
+// for (let i = 0; i < keys.length; i++) {
+//     let key = keys[i];
+//     if (obj[key] % 2 !== 0) {
+//         console.log(`${key}: ${obj[key]}`);
+//     }
+// }
+
+let str = '';
+for (let i = 1; i <= 5; i++) {
+    str += '-' + i;
 }
+str += '-';
+console.log(str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
